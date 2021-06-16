92a9acb1f3254f8efc8c481bd2ab6a3b46ce63e1
ePietrocola <enricopietrocola@gmail.com>
2021-06-16 16:49:14 +0200

    Reworked server creation and reconnection, cleaned up code

--- app.js
+++ app.js
@@ -6,63 +6,37 @@ const fs = require('fs');
 const socketio = require('socket.io')
 const rooms = require('./rooms.js')
 
-//const io = require('socket.io')(http);
-
 let httpsServer;
 let httpServer;
 let io;
 
-const httpsArgs = process.argv.slice(2)
+const args = process.argv.slice(2)
+const key = args[1]
+const cert = args[2]
+const ca = args[3]
+let domain = args[4]
 
-const key = httpsArgs[0]
-const cert = httpsArgs[1]
-const ca = httpsArgs[2]
-let domain = httpsArgs[3]
-
-let totalConnections = 0
-
-const PORT = process.env.PORT || 5000
+let PORT = args[0]
 
 // testing homepage
 app.get('/', (req, res) => {
     res.send('Server up');
 });
 
-// create https server if ssl keys are set properly and passed by argument
-try {
-    if (fs.existsSync(key) && fs.existsSync(cert)) {
-        //file exists
-        httpsServer = https.createServer({
-            key: fs.readFileSync(key, 'utf8'),
-            cert: fs.readFileSync(cert, 'utf8'),
-            //ca: fs.readFileSync(ca, 'utf8') //hide this if your ssl keys don't include ca
-        }, app).listen(443)
-        io = socketio(httpsServer)
-        console.log('Https server running')
-    } else {
-        console.log('Something went wrong with SSL certificates, starting http server')
-        httpServer = http.createServer(app);
-        io = socketio(httpServer)
-        httpServer.listen(PORT)
-    }
-} catch (err) {
-    console.error(err)
-}
+createServer()
 
 io.on('connection', (socket) => {
     console.log("new websocket connection")
+    socket.emit('systemchannel', 'Connected')
 
     socket.on('join', (roomName, password) => {
         socket.join(roomName)
-        totalConnections++
         const room = rooms.addRoom(roomName, password)
 
         if(room.password === password || room.password === ""){
             room.allowedList.indexOf(socket.id) === -1 ? room.allowedList.push(socket.id) : console.log("This item already exists");
-            console.log(room.allowedList)
             socket.to(roomName).emit('datachannel', 'A new user joined the room')
         }
-
     })
 
     socket.on('datachannel', (room, data) => {
@@ -70,11 +44,14 @@ io.on('connection', (socket) => {
         //if user is in allowed list, send data to others
         if (requestedRoom != null) {
             if (requestedRoom.allowedList.indexOf(socket.id) !== -1) {
-                console.log(data)
                 socket.to(room).emit('datachannel', data)
+                console.log(data)
             } else {
                 socket.emit('systemchannel', 'Wrong Password')
             }
+        }
+        else {
+            socket.emit('systemchannel', 'You are not connected to a room')
         }
     })
 
@@ -101,7 +78,28 @@ io.on('connection', (socket) => {
     })
 })
 
-
+function createServer(){
+    // create https server if ssl keys are set properly and passed by argument
+    try {
+        if (fs.existsSync(key) && fs.existsSync(cert)) {
+            //file exists
+            httpsServer = https.createServer({
+                key: fs.readFileSync(key, 'utf8'),
+                cert: fs.readFileSync(cert, 'utf8'),
+                //ca: fs.readFileSync(ca, 'utf8') //hide this if your ssl keys don't include ca
+            }, app).listen(443)
+            io = socketio(httpsServer)
+            console.log('SSL certificates set. Starting https server')
+        } else {
+            console.log('SSL certificates absent. Starting http server')
+            httpServer = http.createServer(app);
+            io = socketio(httpServer)
+            httpServer.listen(PORT)
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
 
 function isJson(str) {
     try {
